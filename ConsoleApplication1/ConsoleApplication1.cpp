a8c4ec14116f359d272438e0953b0eaac5526add
teacher <teacher@303-00>
2025-06-17 18:06:25 +0900

    fix: remove comment

--- ConsoleApplication1/ConsoleApplication1.cpp
+++ ConsoleApplication1/ConsoleApplication1.cpp
@@ -1,7 +1,4 @@
-﻿// ConsoleApplication1.cpp : このファイルには 'main' 関数が含まれています。プログラム実行の開始と終了がそこで行われます。
-//
-
-#include <iostream>
+﻿#include <iostream>
 
 int main()
 {
@@ -9,7 +6,3 @@ int main()
 
     return 0;
 }
-
-// 然
-// プログラムの実行: Ctrl + F5 または [デバッグ] > [デバッグなしで開始] メニュー
-// プログラムのデバッグ: F5 または [デバッグ] > [デバッグの開始] メニュー
